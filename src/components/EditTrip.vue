efd966eee8bc72595a33ea3ff042834652e05571
Wojciech Drazkiewicz <viiragon@gmail.com>
2018-05-22 15:50:29 +0200

    Odczytywanie plików GPS

--- src/components/EditTrip.vue
+++ src/components/EditTrip.vue
@@ -1,40 +1,58 @@
 <template>
-  <div style="width:70vw; margin-left:15vw">
-	<div v-if="data.loaded">
-		<h2>{{ data.trip.title }}</h2>
-		<h2>{{ data.trip.body }}</h2>
-		 <b-table
-				:data="data.list.waypoints"
+	<div style="width:70vw; margin-left:15vw">
+		<div v-if="siteData.loaded">
+			<h2>{{ siteData.trip.name }}</h2>
+			<h2>{{ siteData.trip.description }}</h2>
+			<b-table
+				:data="siteData.trip.waypoints"
 				:columns="columns"
-				:selected.sync="data.list.waypoints[selected]"
+				:selected.sync="siteData.trip.waypoints[selected]"
 				focusable>
 			</b-table>
-	</div>
-	<div v-else>
-		LOADING...
-	</div>
-  </div> 
+		</div>
+		<div v-else>
+			LOADING...
+		</div>
+	</div> 
 </template>
 
 <script>
 import Vue from 'vue'
+
+function findInXML(collection, name) {
+	for (var i = 0; i < collection.length; i++) {
+		if (collection[i].nodeName == name) {
+			return collection[i];
+		}
+	}
+	return null;
+}
+
+function findAllInXML(collection, name) {
+	var list = [];
+	for (var i = 0; i < collection.length; i++) {
+		if (collection[i].nodeName == name) {
+			list.push(collection[i]);
+		}
+	}
+	return list;
+}
+
+var formatDate = function(value) {
+	return value.getDate() + "." + (value.getMonth() + 1) + "." + (value.getYear() + 1900); 
+}
+	
 export default {
 	name: 'TripList',
 	data() {
-		const data = {
-			list: {
-				name: "Okolice Warszawy",
-				description: "Blablabla",
-				start: "2017-08-22T06:11:00.000Z",
-				end: "2017-09-22T06:11:00.000Z",
-				waypoints: []
-			},
-			loaded: false,
-			trip: {}
+		const siteData = {
+			trip: {},
+			loaded: true,
+			tmp: {}
 		}
 
 		return {
-			data,
+			siteData,
 			selected: 1,
 			columns: [
 				{
@@ -52,7 +70,8 @@ export default {
 				{
 					field: 'date',
 					label: 'Date',
-					centered: true
+					centered: true,
+					formatter: formatDate
 				}
 			]
 		}
@@ -62,28 +81,107 @@ export default {
 			Vue.http.get('https://jsonplaceholder.typicode.com/posts/' + index).then(response => {
 				var trip = JSON.parse(response.bodyText);
 				console.log(trip);
-				this.data.trip = trip;
-				this.data.loaded = true;
+				this.siteData.tmp = trip;
+				this.siteData.loaded = true;
 			});
 		},
+		setDummyTrip() {
+			this.siteData.trip = {
+				name: "Okolice Warszawy",
+				description: "Blablabla",
+				start: new Date("2017-08-22T06:11:00.000Z"),
+				end: new Date("2017-09-22T06:11:00.000Z"),
+				waypoints: []
+			};
+		},
 		getRandomWayPoint() {
-			this.data.list.waypoints.push({
+			this.siteData.trip.waypoints.push({
 				"latitude": (Math.random() - 0.5) * 90,
 				"longitude": (Math.random() - 0.5) * 180,
-				"date": "2017-09-22T06:11:00.000Z",
+				"date": new Date("2017-09-22T06:11:00.000Z"),
 				"photos": [],
 				"videos": []
 			});
+		},
+		readGPSFile(fileText) {	
+			try {
+				var xmlDoc;
+				if (window.DOMParser) {
+					var parser = new DOMParser();
+					xmlDoc = parser.parseFromString(fileText, "text/xml");
+				} else { // Internet Explorer
+					xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+					xmlDoc.async = false;
+					xmlDoc.loadXML(fileText);
+				}
+				var trk = findInXML(xmlDoc.getElementsByTagName("gpx")[0].children, "trk");
+				var name = findInXML(trk.children, "name").innerHTML;
+				var wayPoints = [];
+				var trkpts = findAllInXML(findInXML(trk.children, "trkseg").children, "trkpt");
+				var point;
+				for (var i = 0; i < trkpts.length; i++) {
+					point = trkpts[i];
+					wayPoints.push({
+						"latitude": parseFloat(point.attributes.lat.nodeValue),
+						"longitude": parseFloat(point.attributes.lon.nodeValue),
+						"date": new Date(findInXML(point.children, "time").innerHTML),
+						"photos": [],
+						"videos": []
+					});
+				}
+				var trip = {
+					name: name,
+					description: "Blablabla",
+					start: Date.parse("2017-08-22T06:11:00.000Z"),
+					end: Date.parse("2017-09-22T06:11:00.000Z"),
+					waypoints: wayPoints
+				};
+				return trip;
+			} catch(err) {
+				console.log(err);
+				return null;
+			}
 		}
 	},
 	beforeMount(){
 		this.getTrip(this.$route.params.id);
+		/*this.setDummyTrip();
 		this.getRandomWayPoint();
 		this.getRandomWayPoint();
 		this.getRandomWayPoint();
 		this.getRandomWayPoint();
 		this.getRandomWayPoint();
-		this.getRandomWayPoint();
+		this.getRandomWayPoint();*/
+		var trip = this.readGPSFile(	
+			"<gpx>" +
+			"	<metadata>" +
+			"		<link href=\"http://www.garmin.com\">" +
+			"			<text>Garmin International</text>" +
+			"		</link>" +
+			"		<time>2009-10-17T22:58:43Z</time>" +
+			"	</metadata>" +
+			"	<trk>" +
+			"		<name>Example GPX Document</name>" +
+			"		<trkseg>" +
+			"			<trkpt lat=\"47.644548\" lon=\"-122.326897\">" +
+			"				<ele>4.46</ele>" +
+			"				<time>2009-10-17T18:37:26Z</time>" +
+			"			</trkpt>" +
+			"			<trkpt lat=\"47.644548\" lon=\"-122.326897\">" +
+			"				<ele>4.94</ele>" +
+			"				<time>2009-10-17T18:37:31Z</time>" +
+			"			</trkpt>" +
+			"			<trkpt lat=\"47.644548\" lon=\"-122.326897\">" +
+			"				<ele>6.87</ele>" +
+			"				<time>2009-10-17T18:37:34Z</time>" +
+			"			</trkpt>" +
+			"		</trkseg>" +
+			"	</trk>" +
+			"</gpx>"
+		);
+		if (trip != null) {
+			this.siteData.trip = trip;
+		}
 	}
 }
 </script>
