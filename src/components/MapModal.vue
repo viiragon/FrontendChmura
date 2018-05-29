f286984552f05c88224644524aae2e755253132e
parzychu <parzych94@gmail.com>
2018-05-30 01:32:18 +0200

    Dodanie modala do edycji mapy

---
+++ src/components/MapModal.vue
@@ -0,0 +1,129 @@
+
+<template>
+<div>
+{{isCardModalActive}}
+	<form action="">
+		<div class="modal-card" style="width: auto">
+			<header class="modal-card-head">
+				<p v-if="isEdit==false" class="modal-card-title">{{title}}</p>
+				<b-input v-if="isEdit==true" v-model="title"></b-input>
+			</header>
+			<section class="modal-card-body">
+				<div class="content"  v-if="isEdit==true">
+					<h3>Wstaw ten punkt przed
+						
+					</h3>
+					<b-field>
+							<b-select placeholder="Nazwa punktu">
+								<option
+									v-for="option in otherPoints"
+									:value="option.id"
+									:key="option.id">
+									{{ option.name }}
+								</option>
+							</b-select>
+						</b-field>
+				</div>
+				<div class="content" v-if="isEdit==true">
+					<h3>Edytuj zdjęcia</h3>
+				</div>
+				<div class="columns" style="150px">
+					<div class="column" v-for="url in imageUrls" :key="url" @click="isImageModalActive = true">
+						<div :style="{ 'background-image': 'url(' + url + ')', 'height': '150px',  'background-position': 'center', 'background-size': 'cover'}"></div>
+						<b-modal :active.sync="isImageModalActive">
+							<p class="image is-4by3">
+								<img :src="url">
+							</p>
+						</b-modal>
+					</div>     
+					<div class="column" v-if="isEdit==true">
+						<b-upload v-model="dropFiles"
+									multiple
+									drag-drop>
+									<section class="section">
+								<div class="content has-text-centered">
+									<!-- <p>
+										<b-icon
+											icon="upload"
+											size="is-large">
+										</b-icon>
+									</p> -->
+									<p>Drop your files here or click to upload</p>
+								</div>
+							</section>
+						</b-upload>
+					</div>
+				</div>
+				<div class="content" v-if="isEdit==true">
+					<div class="tags">
+						<span v-for="(file, index) in dropFiles"
+							:key="index"
+							class="tag is-info" >
+							{{file.name}}
+							<button class="delete is-small"
+								type="button"
+								@click="deleteDropFile(index)">
+							</button>
+						</span>
+					</div>
+				</div>
+				<div class="content">
+					<h2>Opis</h2>
+					<p v-if="isEdit==false">{{description}}</p>
+					<b-field v-if="isEdit==true">
+						<b-input type="textarea"
+						class="is-info"
+							v-model="description"
+							maxlength="5000"
+							placeholder="Maxlength automatically counts characters">
+						</b-input>
+					</b-field>
+					
+				</div>
+					
+			</section>
+			
+			<footer class="modal-card-foot">
+				<button class="button" type="button" @click="$parent.close()">Zamknij</button>
+				<button v-if="isEdit==true" @click="isEdit=false" class="button is-danger">Usuń</button>
+				<button v-if="isEdit==false" @click="isEdit=true" class="button is-info">Edytuj</button>
+				<button v-if="isEdit==true" @click="isEdit=false" class="button is-info">Zapisz</button>
+			</footer>
+		</div>
+	</form>
+	
+</div>
+</template>
+
+<script>
+export default {
+  name: "MapModal",
+  props: ["email", "password", "isCardModalActive"],
+  methods: {
+	open: function() {
+	  this.isCardModalActive = true;
+	},
+	close: function() {
+	  this.isCardModalActive = false;
+	},
+	deleteDropFile(index) {
+                this.dropFiles.splice(index, 1)
+            }
+  },
+  data() {
+	return {
+		dropFiles: [],
+		otherPoints: [{name: "Punkt Początkowy", id: 44}],
+		isEdit: false,
+		
+                isImageModalActive: false,
+	  imageUrls: [
+		"https://www.sydney.com/sites/sydney/files/styles/gallery_full_width/public/2017-12/Jacarandas%20in%20Spring%20bloom%2C%20Royal%20Botanic%20Garden%2C%20Sydney.jpg?itok=zpohBz_j",
+		"https://www.adventure24.pl/upload/products/australia-objazd--gora-kosciuszki-1354716105.jpg"
+	  ],
+	  title: "Punkt w Australii",
+	  description: "Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl."
+	};
+  }
+};
+</script>
--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,67 +1,103 @@
 <template>
   <section class="section">
-    <h1 class="title">Mapa</h1>
-    <div id="map"></div>
+	<h1 class="title">Mapa</h1>
+	<div id="map"></div>
+    
+        <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
+            <map-modal :isCardModalActive.sync="isCardModalActive" has-modal-card>
+        </map-modal>
+        
+        </b-modal>
   </section>
 </template>
 
 <script>
+
+import MapModal from "./MapModal";
+
 export default {
+
   name: "MapView",
+  components: {
+        'map-modal': MapModal
+    },
   created: function() {
-    function placeMarker(location, map) {
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
+      var flightPlanCoordinates = [
+      ];
+        var self = this;
+
+	function placeMarker(location, map) {
+	  var marker = new google.maps.Marker({
+		position: location,
+		map: map
       });
+      
+      flightPlanCoordinates.push(location);
+    var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+      
+      flightPath.setMap(map)
+      marker.addListener("click", self.activateModal);
+	  console.log("click", location);
+	}
 
-      console.log("click", location);
-    }
+	function initMap() {
+	  var uluru = { lat: -25.363, lng: 131.044 };
+	  var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: uluru
+	  });
+	  var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	  });
+	  marker.addListener("click", self.activateModal);
 
-    function initMap() {
-      var uluru = { lat: -25.363, lng: 131.044 };
-      var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru
-      });
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
+flightPlanCoordinates.push(
+          new google.maps.LatLng(-25.363, 131.044),
+          )
+	  map.addListener("click", function(event) {
+		placeMarker(event.latLng, map);
+	  });
+	//   var image = {
+	// 	url: "https://www.wykop.pl/cdn/c3201142/comment_PsKw6GPbr0qiHWADi51CcZmnSz7I7tvK.jpg",
+	// 	size: new google.maps.Size(32, 32)
+	// 	}
+	//   var beachMarker = new google.maps.Marker({
+	// 	position: { lat: -33.89, lng: 151.274 },
+	// 	map: map,
+	// 	icon: image
+    //   });
+	}
 
-      marker.addListener("click", function() {
-        map.setZoom(8);
-        map.setCenter(marker.getPosition());
-      });
+	window.initMap = initMap;
 
-      map.addListener("click", function(event) {
-        placeMarker(event.latLng, map);
-      });
-      var image = {
-        url: "https://www.wykop.pl/cdn/c3201142/comment_PsKw6GPbr0qiHWADi51CcZmnSz7I7tvK.jpg",
-        size: new google.maps.Size(32, 32)
-        }
-      var beachMarker = new google.maps.Marker({
-        position: { lat: -33.89, lng: 151.274 },
-        map: map,
-        icon: image
-      });
-    }
+	var mapKey = document.createElement("script");
+	mapKey.setAttribute(
+	  "src",
+	  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDCVCc8UDPw3icWW8kDc89JPqmwsS9FqWQ&callback=initMap"
+	);
+	mapKey.setAttribute("type", "text/javascript");
 
-    window.initMap = initMap;
-
-    var mapKey = document.createElement("script");
-    mapKey.setAttribute(
-      "src",
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDCVCc8UDPw3icWW8kDc89JPqmwsS9FqWQ&callback=initMap"
-    );
-    mapKey.setAttribute("type", "text/javascript");
-
-    document.body.appendChild(mapKey);
+	document.body.appendChild(mapKey);
   },
-  data: function() {
-    return {};
-  }
+  methods: {
+        activateModal: function() {
+          
+          console.log(this.isCardModalActive)
+          this.isCardModalActive = true;
+
+        }
+    },
+  data() {
+        return {
+            isCardModalActive: false
+        }
+    }
 };
 </script>
 <style>
