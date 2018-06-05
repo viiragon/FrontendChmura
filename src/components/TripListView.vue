4727551ecf941ca755f919a87b800dc4b94f7877
Zuzanna ???wikli??ska <zuuu.bu@gmail.com>
2018-06-05 23:19:46 +0200

    trips z api

--- src/components/TripListView.vue
+++ src/components/TripListView.vue
@@ -2,10 +2,26 @@
 	<section class="section trips">
 		<h1 class="title">Trips</h1>
 		<b-table
-			:data="data"
-			:columns="columns"
+			:data="trips"
 			focusable
 			@dblclick="rowClickedHandler">
+			<template slot-scope="props">
+				<b-table-column field="tripId" :visible="false">
+					{{ props.row.tripId }}
+				</b-table-column>
+
+				<b-table-column field="name" label="Name" width="200" centered>
+					{{ props.row.name }}
+				</b-table-column>
+
+				<b-table-column field="start" label="Start" width="250" centered>
+					{{ new Date(props.row.start).toLocaleDateString()}}
+				</b-table-column>
+
+				<b-table-column field="end" label="End" width="250" centered>
+					{{ new Date(props.row.end).toLocaleDateString()}}
+				</b-table-column>
+			</template>
 		</b-table>
 		</br>
 		<div id="new-trip-div">
@@ -17,61 +33,36 @@
 </template>
 
 <script>
+
+	import http from "./HttpService"
+
+	const trips = [];
+
 	export default {
 		name: 'TripListView',
 		data() {
-			const data = [
-				{ 'id': 1, 'name': 'Okolice Warszawy', 'description': 'Blablabla', 'start': "2017-08-22 06:11:00.000Z", 'end': "2017-09-22 06:11:00.000Z"},
-				{ 'id': 2, 'name': 'Okolice Warszawy', 'description': 'Blablabla', 'start': "2017-08-22 06:11:00.000Z", 'end': "2017-09-22 06:11:00.000Z" },
-				{ 'id': 3, 'name': 'Okolice Warszawy', 'description': 'Blablabla', 'start': "2017-08-22 06:11:00.000Z", 'end': "2017-09-22 06:11:00.000Z" }
-			]
-			return {
-				data,
-				selected: data[0],
-				columns: [
-					{
-						field: 'id',
-						label: 'ID',
-						width: '40',
-						numeric: true,
-						centered: true
-					},
-					{
-						field: 'name',
-						label: 'Name',
-						width: '200',
-						centered: true
-					},
-					{
-						field: 'description',
-						label: 'Description',
-						width: '300',
-						centered: true
-					},
-					{
-						field: 'start',
-						label: 'Start',
-						width: '250',
-						centered: true
-					},
-					{
-						field: 'end',
-						label: 'End',
-						width: '250',
-						centered: true
-					}
-				]
+			 return {
+			 trips
 			}
 		},
 		methods: {
 			rowClickedHandler(row){
-				window.location = '/#/trip/' + row.id;
+				window.location = '/#/trip/' + row.tripId;
 			}
+		},
+		mounted () {
+			var vm = this;
+			http.get("trips", function(response) {
+				vm.trips = response.data.content;
+			}, function(error) {
+				console.log("error: " + error);
+			});
 		}
 	}
 
 </script>
-	<style scoped>
+
+<style scoped>
 	#new-trip-div{
 		text-align: center;
 	}
