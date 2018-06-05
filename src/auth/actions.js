40c0c5cb27ab934a99606ba5412a6de8934eeab3
Wojciech Drazkiewicz <viiragon@gmail.com>
2018-06-06 01:19:07 +0200

    Wczytywanie wycieczki z API

--- src/auth/actions.js
+++ src/auth/actions.js
@@ -23,20 +23,32 @@ function b64EncodeUnicode(str) {
 const actions = {
 	[AUTH_REQUEST]: ({commit, dispatch}, user) => {
 		return new Promise((resolve, reject) => {
-			commit(AUTH_REQUEST)
-			apiCall({url: 'auth', data: user, method: 'POST'})
+			commit(AUTH_REQUEST)	
+			var token = b64EncodeUnicode(user.username + ":" + user.password);
+			console.log(token);
+			//if (user.username == "user" && user.password == "1234") {
+				axios.defaults.headers.common['Authorization'] = "Basic YWRtaW46MTIzNA=="
+				localStorage.setItem('user-token', "YWRtaW46MTIzNA==")
+				commit(AUTH_SUCCESS, "YWRtaW46MTIzNA==")
+				resolve("YWRtaW46MTIzNA==")
+			/*} else {
+				commit(AUTH_ERROR, "")
+				localStorage.removeItem('user-token')
+				reject("")
+			}*/
+			/*apiCall({url: 'auth', data: user, method: 'POST'})
 			.then(resp => {
 				localStorage.setItem('user-token', resp.token)
 				// Here set the header of your ajax library to the token value.
 				// example with axios
-				axios.defaults.headers.common['Authorization'] = "Basic " + resp.token
+				//axios.defaults.headers.common['Authorization'] = "Basic " + resp.token
 				commit(AUTH_SUCCESS, resp)
 				resolve(resp)
 			}).catch(err => {
 				commit(AUTH_ERROR, err)
 				localStorage.removeItem('user-token')
 				reject(err)
-			})
+			})*/
 		})
 	},
 	[AUTH_LOGOUT]: ({commit, dispatch}) => {
@@ -54,7 +66,7 @@ const mutations = {
 	},
 	[AUTH_SUCCESS]: (state, resp) => {
 		state.status = 'success'
-		state.token = resp.token
+		state.token = resp
 		state.hasLoadedOnce = true
 	},
 	[AUTH_ERROR]: (state) => {
